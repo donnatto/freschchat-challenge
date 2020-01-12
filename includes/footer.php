6512f3e8fd52050c04491453c8149244ec91ae72
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-11 21:34:33 -0500

    add freshchat messenger

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,16 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="vendor/js/bootstrap.min.js"></script>
+<!-- Freshchat -->
+<script>
+  function initFreshChat() {
+    window.fcWidget.init({
+      token: "ea501f35-eb44-4af2-851f-78f45fdff8bd",
+      host: "https://wchat.freshchat.com"
+    });
+  }
+  function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
+</script>
 
 </body>
 </html>
