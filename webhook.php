5fee879d9a34d081294f77252094fcd708cda28a
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-12 22:22:07 -0500

    edit webhook

--- webhook.php
+++ webhook.php
@@ -1,7 +1,13 @@
 <?php
 
+if (file_exists('request.log')) {
+  $log = readfile('request.log');
+  var_dump($log);
+}
+
 header('Content-Type: application/json');
 $request = file_get_contents('php://input');
 $req_dump = print_r( $request, true );
 $fp = file_put_contents( 'request.log', $req_dump );
+
 ?>
