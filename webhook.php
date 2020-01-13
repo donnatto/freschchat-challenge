69c1c017d2433976c476897647ca6ed5d8add30f
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-12 23:34:40 -0500

    check freshchat auth in webhook

--- webhook.php
+++ webhook.php
@@ -1,13 +1,22 @@
 <?php
+$auth_token = $_SERVER['HTTP_X_FRESHCHAT_SIGNATURE'];
+$wh_auth = getenv('WH_AUTH');
+$api_token = getenv('FRESHCHAT_API_TOKEN');
+$url='https//api.freshchat.com/v2/';
 
 if (file_exists('request.log')) {
   $log = readfile('request.log');
   var_dump($log);
 }
 
-header('Content-Type: application/json');
-$request = file_get_contents('php://input');
-$req_dump = print_r( $request, true );
-$fp = file_put_contents( 'request.log', $req_dump );
+if ($auth_token == $wh_auth) {
+  header('Content-Type: application/json');
+  $request = file_get_contents('php://input');
+  $req_dump = print_r( $request, true );
+  $fp = file_put_contents( 'request.log', $req_dump );
+} else {
+  $request = "you don't have access to this";
+  $fp = file_put_contents($request, true);
+}
 
 ?>
