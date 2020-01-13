7b98b8983da9cf74547daa6806aa65c9b102fc5b
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-13 13:58:27 -0500

    dump request log in webhook

--- webhook.php
+++ webhook.php
@@ -9,16 +9,7 @@ if (file_exists('request.log')) {
   var_dump($log);
 }
 
-header('Content-Type: application/json');
 $request = file_get_contents('php://input');
-// Verificamos el request con la firma y nuestra clave publica
-$verify = openssl_verify($request, $signature, $public_key);
-if ($verify == 1) {
-  $req_dump = print_r( $request, true );
-  $fp = file_put_contents('request.log', $req_dump );
-} else {
-  $message = "You don't have permission to do this";
-  $fp = file_put_contents('request.log', $message);
-}
+$fp = file_put_contents('request.log', $request);
 
 ?>
